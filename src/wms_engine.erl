14ec7db100778e4042b530a2fcda56a8cc886282
Makra Attila <makraat@ext.otpbank.hu>
2019-11-14 21:46:26 +0100

    Modify configuration settings

--- src/wms_engine.erl
+++ src/wms_engine.erl
@@ -12,22 +12,5 @@
 -include("wms_engine.hrl").
 
 %% API
--export([load_config/0]).
-
--spec load_config() ->
-  ok.
-load_config() ->
-  load_config(wms_cfg:get(?APP_NAME, load_config, true)).
-
-%% =============================================================================
-%% Private functions
-%% =============================================================================
-
--spec load_config(boolean()) ->
-  ok.
-load_config(true) ->
-  Path = filename:join(code:priv_dir(?APP_NAME), "wms_engine.config"),
-  ok = wms_cfg:overload_config(wms_cfg:get_mode(), [Path]);
-load_config(_) ->
-  ok.
+-export([]).
 
